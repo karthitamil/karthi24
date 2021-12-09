4be645baac367eb486609377e721c8608829ffa6
karthitamil <karthisekartamil2121@gmail.com>
2021-12-10 01:58:38 +0530

    karthi task is completed

---
+++ src/org/edu/Student.java
@@ -0,0 +1,18 @@
+package org.edu;
+
+public class Student {
+	public static void main(String[] args) {
+		for (int i = 0; i<5; i++) {
+			
+			if (i==3) {
+				continue;
+				
+			}
+			System.out.println(i);
+			
+			
+		}
+		System.out.println("finish");
+	}
+
+}
